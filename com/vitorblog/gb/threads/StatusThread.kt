e6b079ff7bec1bbb741bef5449e2303ce2e448d6
RodrigoCarvalho31 <39930122+RodrigoCarvalho31@users.noreply.github.com>
2019-03-04 18:36:09 -0300

    Change random to constant

--- com/vitorblog/gb/threads/StatusThread.kt
+++ com/vitorblog/gb/threads/StatusThread.kt
@@ -10,16 +10,17 @@ class StatusThread(j:Bot) : TimerTask() {
     var bot = j
     var status = arrayListOf("Minecraft", "JetBrains IDE")
     var statusTwitch = arrayListOf("a careca do Rosset", "JetBrains IDE")
+    val RANDOM = Random()
 
     override
     fun run() {
         try{
         var chance = Random().nextInt(100)
         if (chance >= 20){
-            var entry = status.get(Random().nextInt(status.size))
+            var entry = status.get(RANDOM.nextInt(status.size))
             jda.presence.game = Game.of(Game.GameType.DEFAULT, entry)
         } else {
-            var entry = statusTwitch.get(Random().nextInt(statusTwitch.size))
+            var entry = statusTwitch.get(RANDOM.nextInt(statusTwitch.size))
             jda.presence.game = Game.of(Game.GameType.STREAMING, entry, "https://www.twitch.tv/focablog")
         }
         }catch (E:Exception){bot.error(E)}
