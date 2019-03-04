bda8bffdb2029775432ab5955d9034ab54f80342
Vitor <35616041+VitorBlog@users.noreply.github.com>
2019-03-04 18:04:31 -0300

    Update Main.kt

--- com/vitorblog/gb/Main.kt
+++ com/vitorblog/gb/Main.kt
@@ -1,17 +1,9 @@
 package com.vitorblog.gb
 
-import java.lang.Exception
-
 class Main {companion object {
-
-    var bot:Bot? = null
-
     @JvmStatic
     fun main(args:Array<String>){
-        bot = Bot(args[0])
-        try { bot!!.start() }catch (E:Exception){ bot!!.error(E) }
+        val bot = Bot(args[0])
         println("${bot!!.jda!!.selfUser.name} started")
     }
-
-
 }}
