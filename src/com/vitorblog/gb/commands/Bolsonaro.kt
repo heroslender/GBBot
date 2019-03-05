0913310ef717ad084badf0403564b5aeaa0ee000
vitorblog <vitorblog12@gmail.com>
2019-03-05 17:46:25 -0300

    Refresh files

--- src/com/vitorblog/gb/commands/Bolsonaro.kt
+++ src/com/vitorblog/gb/commands/Bolsonaro.kt
@@ -12,7 +12,7 @@ class Bolsonaro : CMD {
     override var mc: TextChannel? = null
     override var g:Guild? = null
     var webhook = WebHook("https://discordapp.com/api/webhooks/552577918006722561/fXQhWdWRiz4_ZkaYAnN2SniSgh8U0mAgxQf1r_bfmp99ZgGmrOML9LFsBhXhpPmuDlni")
-    val msgs = arrayListOf("Benino veste azul Benina veste rosa", "Tem que se fude e acabo", "Skripter bom é skripter banido", "Se tem tinta na frente já sei que laga", "", "")
+    val msgs = arrayListOf("Benino veste azul Benina veste rosa", "Tem que se fude e acabo", "Skripter bom é skripter banido", "Se tem tinta na frente já sei que laga", "taokey", "$taokey")
 
     override
     fun execute(p:User, msg:Message, a:List<String>, mc:TextChannel, g:Guild, e: GuildMessageReceivedEvent) {
